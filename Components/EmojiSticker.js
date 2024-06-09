1b4f311c78050691cb5984d8ae7d9e170a598a46
Zaidh Imran <zaidhimran2000@gmail.com>
2024-06-08 23:03:10 -0230

    implement pan gesture

--- Components/EmojiSticker.js
+++ Components/EmojiSticker.js
@@ -5,6 +5,14 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 export default function EmojiSticker({imageSize, stickerSource}) {
 
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const drag = Gesture.Pan()
+    .onChange((event) => {
+      translateX.value += event.changeX;
+      translateY.value += event.changeY;
+    });
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -21,8 +29,22 @@ export default function EmojiSticker({imageSize, stickerSource}) {
     };
   });
 
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value
+        },
+        {
+          translateY: translateY.value
+        }
+      ]
+    };
+  });
+
   return (  
-        <View style={{top: -350}}>
+        <GestureDetector gesture={drag}>
+          <Animated.View style={[containerStyle, {top: -350}]}>
             <GestureDetector gesture={doubleTap}>
               <Animated.Image
                   source={stickerSource}
@@ -30,6 +52,7 @@ export default function EmojiSticker({imageSize, stickerSource}) {
                   style={[imageStyle, {width: imageSize, height: imageSize}]}
               />
             </GestureDetector>
-        </View>
+        </Animated.View>
+        </GestureDetector>
   ) 
 }
